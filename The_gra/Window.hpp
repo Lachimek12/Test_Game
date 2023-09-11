8eba8e99b938b3c81a914e8977d3eff0f5ce1b3b
micha <micha@DESKTOP-CH31M67>
2023-09-11 16:22:24 +0200

    Add project files.

---
+++ The_gra/Window.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<SDL.h>
+
+class Window
+{
+public:
+	//The window we'll be rendering to
+	SDL_Window* window;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface;
+
+public:
+	Window();
+	~Window();
+
+private:
+
+};
+
+Window::Window()
+{
+}
+
+Window::~Window()
+{
+}
